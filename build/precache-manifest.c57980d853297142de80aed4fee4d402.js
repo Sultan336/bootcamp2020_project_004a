8c9ba858678692600cf92b060b382f57a66de1ce
Sultan <m.sultan338@gmail.com>
2021-04-26 21:26:31 +0500

    Initial commit

---
+++ build/precache-manifest.c57980d853297142de80aed4fee4d402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2333a20070ea4386ae9cc6578d1a23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f1d68e8c571307b084",
+    "url": "/static/css/main.de4b4761.chunk.css"
+  },
+  {
+    "revision": "16dea9a7795c0c35578c",
+    "url": "/static/js/2.eb59f496.chunk.js"
+  },
+  {
+    "revision": "e1f1d68e8c571307b084",
+    "url": "/static/js/main.22bbf979.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
